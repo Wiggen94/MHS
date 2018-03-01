e1311406bb49929dbeb5b26a65ee142d39ff9c22
hannafahlvik <36951945+hannafahlvik@users.noreply.github.com>
2018-03-01 10:57:34 +0100

    Add files via upload
    Endret IP fra lokal til tall

--- config/auth.js
+++ config/auth.js
@@ -6,14 +6,14 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '363844550760695', // your App ID
         'clientSecret'  : 'ce9ef7532aa6251345808ef71cae84dd', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'   : 'http://10.22.187.83:8080/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     },
     'googleAuth' : {
         'clientID'      : '559979724442-e4cl7fssbai3pb009ivrcuhrha88ikh1.apps.googleusercontent.com',
         'clientSecret'  : '3WaasTfJmXxBCsOpErFxqLIm',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'http://10.22.187.83:8080/auth/google/callback'
     }
 
 };
