8d9f9252909e390101c8361a73c7cec87b331d40
Gjermund Wiggen <gjermundwiggen@gmail.com>
2018-03-04 20:42:45 +0100

    Startet på design og opprettet egendefinert CSS-fil

--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
+var express  = require('./node_modules/express/index.js');
 var app      = express();
 var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
+var mongoose = require('./node_modules/mongoose/lib/index.js');
+var passport = require('./node_modules/passport/lib/index.js');
+var flash    = require('./node_modules/connect-flash/lib/index.js');
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var morgan       = require('./node_modules/morgan/index.js');
+var cookieParser = require('./node_modules/cookie-parser/index.js');
+var bodyParser   = require('./node_modules/body-parser/index.js');
+var session      = require('./node_modules/express-session/index.js');
 
 var configDB = require('./config/database.js');
 
@@ -24,15 +24,20 @@ require('./config/passport')(passport); // pass passport for configuration
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('view engine', './node_modules/ejs/lib/ejs.js'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch', saveUninitialized: true, resave: true })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(express.static(__dirname + '/views'));
+
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
